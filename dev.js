b7c367a5e740c2b513f45580904b31f5c3efabc5
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-03-15 15:56:00 +0000

    Improve dev script messages

--- dev.js
+++ dev.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
+
 // Runs Place in development mode using Nodemon
 // to manage restarts on code changes.
 import nodemon from 'nodemon'
-import clr from './lib/clr.js'
+import chalk from 'chalk'
 
 const __dirname = new URL('.', import.meta.url).pathname
 
@@ -10,23 +12,24 @@ function prettyFilePath (filePath) {
 }
 
 nodemon({ script: 'bin/place.js' })
-  .on('restart', (changedFiles) => {
-    const numberOfChangedFiles = changedFiles.length
-    if (numberOfChangedFiles > 0) {
-      const firstFile = clr(prettyFilePath(changedFiles[0]), 'green')
-      const otherFilesIfAny = numberOfChangedFiles > 1 ? `(${clr(` and ${numberOfChangedFiles - 1} others`, 'cyan')})` : ''
+.on('restart', (changedFiles) => {
+  const numberOfChangedFiles = changedFiles.length
+  if (numberOfChangedFiles > 0) {
+    const firstFile = chalk.green(prettyFilePath(changedFiles[0]))
+    const otherFilesIfAny = numberOfChangedFiles > 1 ? `(${chalk.cyan(` and ${numberOfChangedFiles - 1} others`)})` : ''
 
-      console.log(`   🤓️    ❨Place❩ Code changed in ${firstFile}${otherFilesIfAny}.\n`)
-      console.log('   🔄    ❨Place❩ Restarting server…\n\n')
-    }
-  })
-  .on('exit', (reason) => {
-    if (reason === undefined) {
-      // This is what we get if the person signals they want to quit
-      // using Ctrl+C. So, actually quit. Otherwise, let Nodemon manage it.
-      process.exit(0)
-    }
-  })
-  .on('crash', () => {
-    console.log('DEV crashed')
-  })
+    console.log(`\n   🤓️    ❨Place❩ Code changed in ${firstFile}${otherFilesIfAny}.\n`)
+    console.log('   🔄    ❨Place❩ Restarting server…\n\n')
+  }
+})
+.on('exit', (reason) => {
+  if (reason === undefined) {
+    // This is what we get if the person signals they want to quit
+    // using Ctrl+C. So, actually quit. Otherwise, let Nodemon manage it.
+    process.exit(0)
+  }
+})
+.on('crash', () => {
+  console.log(`\n   🤯️    ${chalk.red('❨Place❩ Crashed!')}`)
+  console.log(chalk.yellow.italic('         Waiting for code change to restart…'))
+})
