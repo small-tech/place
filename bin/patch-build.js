ff3209a212c31accd7768b221ba809b11d54739f
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-03-26 12:10:52 +0000

    Build is now minified and placed in ../dist/

--- bin/patch-build.js
+++ bin/patch-build.js
@@ -3,7 +3,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
-const build = path.join(__dirname, '..', 'place.js')
+const build = path.join(__dirname, '..', '..', 'dist', 'place.mjs')
 const unpatchedBuild = fs.readFileSync(build, 'utf-8')
-const patchedBuild = unpatchedBuild.replace('#!/usr/bin/env node', '#!/usr/bin/env node\n\nconst require = createRequire(import.meta.url)\n')
+const patchedBuild = unpatchedBuild.replace('#!/usr/bin/env node\n', '#!/usr/bin/env node\nimport{createRequire}from\'module\';const require=createRequire(import.meta.url);')
 fs.writeFileSync(build, patchedBuild, 'utf-8')
